80365627bfe6bef672aa84e11b9fa80fee5b9081
Tygran <tpetro@softserveinc.com>
2022-09-21 11:36:45 +0300

    updated main file

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ public class Main {
 
         List<String> list = new ArrayList<>();
         list.add("text1");
+        list.add("text1.5");
         list.add("text2");
+        list.add("text2.5");
         list.add("text3");
         for (String string : list) {
             System.out.println(string);
